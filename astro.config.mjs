13b9f44e8f5a43b33eb2a2b5407aaaa92eacff65
xScripted <miquel97bdn@gmail.com>
2023-04-03 23:28:04 +0200

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +11,12 @@ export default defineConfig({
     css: {
       preprocessorOptions: {
         sass: {
-          additionalData: `@import "./src/assets/variables.scss";`,
-        },
-      },
-    },
+          additionalData: `@import "./src/assets/variables.scss";`
+        }
+      }
+    }
   },
-})
+  adapter: node({
+    mode: "standalone"
+  })
+});
