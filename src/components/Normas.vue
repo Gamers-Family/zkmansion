7d34d2d2bee88058e65c3fac57cfedb2e529715d
Pyrogeno <Kevin_kap1993@hotmail.com>
2023-05-03 20:21:22 +0200

    Modificacion Normas.vue y todas las paginas de normas añadidas

--- src/components/Normas.vue
+++ src/components/Normas.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
 const props = defineProps({
   normas: {
-    type: Object,
-    required: true,
+    type: Array, // Cambiado de Object a Array
+    required: false,
+  },
+  reglas: {
+    type: Array, // Cambiado de Object a Array
+    required: false,
   },
 })
 </script>
 
 <template>
-  <div class="norma" v-for="(norma, index) in normas" :key="norma">
+ <div class="regla" v-for="(regla, index) in reglas" :key="index">{{ regla }}</div>
+  <div class="norma" v-for="(norma, index) in normas" :key="index">
     <span class="numero">{{ index + 1 }} - </span>{{ norma }}
   </div>
 </template>
 
 <style lang="scss">
+.regla{
+  margin-bottom: 5px;
+}
+
 .norma {
   margin-bottom: 5px;
 
