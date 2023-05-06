119bee9c4d75b06b9a8ed5135663daf03fce2eb3
xScripted <miquel97bdn@gmail.com>
2023-05-06 15:20:07 +0200

    historial + fixes

---
+++ src/components/HistorialItem.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  zkoins: Number,
+  concepto: String,
+})
+</script>
+
+<template>
+  <div class="historial">
+    <div class="historial__diferencia--rojo">{{ zkoins }} ZK</div>
+    <div class="historial__concepto">{{ concepto }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.historial {
+  display: grid;
+  grid-template-columns: 40px 1fr;
+  font-size: 14px;
+  padding: 10px;
+
+  &__diferencia {
+    &--verde {
+      color: rgb(0, 206, 0);
+    }
+
+    &--rojo {
+      color: red;
+    }
+  }
+
+  &__concepto {
+    color: rgb(207, 207, 207);
+  }
+}
+</style>
